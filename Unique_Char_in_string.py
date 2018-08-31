9db0b23d2daa2a89fbcfddb6b919f02d7401e7be
Jordan Williams <40617753+williamsjr@users.noreply.github.com>
2018-08-30 21:25:18 -0400

    Add files via upload

---
+++ Unique_Char_in_string.py
@@ -0,0 +1,16 @@
+class UniqueChars(object):
+    """
+    Return weather or not a string has all unique characters.
+    
+    None -> False
+    '' -> True
+    'foo' -> False
+    'bar' -> True
+    """
+    
+    def has_unique_chars(self, string):
+    if string is None or '':
+        return False
+    else:
+        len(set(string) == len(string)
+    return True
